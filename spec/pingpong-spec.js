9724d88189f9e7f3f729cbfc6bb54a53eced348f
Guest User <Guest@Epicodus-6B.local>
2017-10-10 16:44:54 -0700

    jasmine spec passing

--- spec/pingpong-spec.js
+++ spec/pingpong-spec.js
@@ -5,6 +5,6 @@ describe('Calculator', function () {
 
   it('should test whether a number returns a value', function() {
     var test1 = new Calculator()
-    expect(test1.pingPong(1)).toEqual([1]);
+    expect(test1.pingPong(5)).toEqual([1,2, 'ping',4,'pong']);
   });
 });
